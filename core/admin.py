a18f8e5419033add3f18d3ce99d658db761b1710
Malachy K. Madu <kidakwesta9@gmail.com>
2025-04-12 07:44:49 +0100

    admin page set-up

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Farmer, Buyer, Driver 
+from .models import Farmer, Buyer, Driver, UserManager 
+#from vege_tracker.models  import Vegetable,Order, OrderItem
 # Register your models here.
 #admin.site.register(UserManager)
 admin.site.register(Farmer)
 admin.site.register(Buyer)
 admin.site.register(Driver)
-# admin.site.register(Vegetable)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
+#admin.site.register(Vegetable)
+#admin.site.register(Order)
+#admin.site.register(OrderItem)
--- vege_tracker/admin.py
+++ vege_tracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Order, Vegetable, OrderItem 
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(Vegetable)
+admin.site.register(OrderItem)
